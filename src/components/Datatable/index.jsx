c20c83b334cf7fbde2bdd2b5374bfc63b436c719
scoobytux <tu.lna07@gmail.com>
2022-08-20 21:53:47 +0700

    feat: delete row data table

--- src/components/Datatable/index.jsx
+++ src/components/Datatable/index.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 // Material UI
 import { DataGrid } from "@mui/x-data-grid";
@@ -13,17 +14,28 @@ import {
 import "./style.scss";
 
 const Datatable = () => {
+  const [data, setData] = useState(rows);
+
+  const handleDelete = id => {
+    setData(data.filter(item => item.id !== id));
+  };
+
   const actionColumn = [
     {
       field: "action",
       headerName: "Action",
       width: 200,
-      renderCell: () => (
+      renderCell: params => (
         <div className="cell-actions">
           <Link to="/users/test" style={{ textDecoration: "none" }}>
             <div className="cell-action btn-view">View</div>
           </Link>
-          <div className="cell-action btn-delete">Delete</div>
+          <div
+            className="cell-action btn-delete"
+            onClick={() => handleDelete(params.row.id)}
+          >
+            Delete
+          </div>
         </div>
       ),
     },
@@ -39,7 +51,7 @@ const Datatable = () => {
       </div>
       <DataGrid
         className="data-grid"
-        rows={rows}
+        rows={data}
         columns={columns.concat(actionColumn)}
         pageSize={5}
         rowsPerPageOptions={[5]}
